1385de4c315c40577a27eb07ed757a541ca26ed5
reddyakash13 <reddyakash13@gmail.com>
2018-05-27 20:39:52 +0530

    First draft version of Flesch-Kincaid algorithm
    Change-Id: I7a3df6554449c704383f0231ca2a1815d154a638
    Signed-off-by: reddyakash13 <reddyakash13@gmail.com>

---
+++ src/easytext/javamodularity/easytext/Main.java
@@ -0,0 +1,80 @@
+package easytext.javamodularity.easytext;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		if (args.length == 0) {
+			System.out.println("Welcome to EasyText. Please provide a filename as input argument");
+			return;
+		}
+		
+		Path path = Paths.get(args[0]);
+		System.out.println("Reading: " + path);
+		String text = new String(Files.readAllBytes(path), Charset.forName("UTF8"));
+		
+		List<List<String>> sentences = toSentences(text);
+		
+		System.out.println("Flesch-Kincaid: " + analyze(sentences));
+
+	}
+	
+	public static List<List<String>> toSentences(String text) {
+		String removeBreaks = text.replaceAll("\\r?\\n", " ");
+		ArrayList<List<String>> sentences = new ArrayList<>();
+		for (String rawSentence : removeBreaks.split("[\\.\\?\\!]")) {
+			List<String> words = toWords(rawSentence);
+			if (words.size() > 0) {
+				sentences.add(words);
+			}
+		}
+		return sentences;
+	}
+	
+	private static List<String> toWords(String sentence) {
+		String[] rawWords = sentence.split("\\s+");
+		List<String> words = new ArrayList<>();
+		for (String rawWord : rawWords) {
+			String word = rawWord.replaceAll("\\W", "");
+			if (word.length() > 0) {
+				words.add(word);
+			}
+		}
+		return words;
+	}
+
+	public static double analyze(List<List<String>> sentences) {
+		float totalSentences = sentences.size();
+		float totalWords = sentences.stream().mapToInt(sentence -> sentence.size()).sum();
+		float totalSyllables = sentences.stream()
+				.flatMapToInt(sentence -> sentence.stream().mapToInt(word -> countSyllables(word)))
+				.sum();
+		return 206.835 - 1.015 * (totalWords / totalSentences) - 84.6 * (totalSyllables / totalWords);
+	}
+	
+	public static int countSyllables(String word) {
+		int syllables = 0;
+		boolean prevNonVowel = false;
+		for (int i = 0; i < word.length(); i++) {
+			boolean isVowel = isVowel(word.toLowerCase().charAt(i));
+			if (prevNonVowel && isVowel && i != word.length() -1) {
+				syllables++;
+			}
+			prevNonVowel = !isVowel;
+		}
+		syllables = syllables == 0 ? 1 : syllables;
+		return syllables;
+	}
+	
+	private static boolean isVowel(char letter) {
+		return letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u';
+	}
+}
---
+++ src/easytext/javamodularity/module-info.java
@@ -0,0 +1,5 @@
+module easytext {
+
+	requires java.base;
+	
+}
